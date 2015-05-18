ed64070c93abd665b5e303061d0d864b29e0904f
Patrick Gerdsmeier <pg@binaerraum.de>
2015-05-18 10:05:32 +0200

    sha256

--- arm-toolchain.rb
+++ arm-toolchain.rb
@@ -4,7 +4,7 @@ class ArmToolchain < Formula
   homepage 'https://launchpad.net/gcc-arm-embedded'
   version '2015.03.20'
   url 'https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-update/+download/gcc-arm-none-eabi-4_9-2015q1-20150306-mac.tar.bz2'
-  md5 'e3d92e5eaac7f9b0c20bf69822a434dd'
+  sha256 '049469f06c35d1a7c6d333236bc62ed340531c4377b4e16ce8c5aa8d134e8e8b'
 
   def install 
     ohai 'Copying binaries...'
